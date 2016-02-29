fd7bf04acae46571d2d5b84b0eca777a505af1ee
Jugulaire <julien.guerder@icloud.com>
2016-02-29 15:46:13 +0100

    Script changes

--- memory.sh
+++ memory.sh
@@ -8,16 +8,16 @@ rambuffer=$(cat /proc/meminfo | grep -i "^buffers" |tr "A-z : " " " | sed -e "s/
 
 ramtotal=$(cat /proc/meminfo | grep -i "^memtotal" | tr "A-z :" " " | sed -e "s/ *//g")
 let "ramLibre=$ramLibre/1024"
-echo "ram libre $ramLibre" 
+echo "ram free $ramLibre" 
 let "ramcached=$ramcached/1024"
 echo "ram cached $ramcached"
 let "rambuffer=$rambuffer/1024"
-echo "rambuffer $rambuffer"
+echo "ram buffer $rambuffer"
 let "ramtotal=$ramtotal/1024"
 echo "ramtotal $ramtotal"
 
 let "memnonused=($ramLibre + $ramcached + $rambuffer)"
-echo "ram non utiliser" $memnonused
+echo "ram not in use" $memnonused
 
 let "memused=$ramtotal-$memnonused"
-echo "ram utiliser" $memused
+echo "ram used" $memused
